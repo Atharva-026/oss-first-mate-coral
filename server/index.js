b89dcb6dfd65aba03d69c0e3690a0872b58a5e89
atharvanaik <atharvanaik026@gmail.com>
2026-06-09 01:16:01 +0530

    Docker fully working, Ubuntu 24.04, Coral v0.4.2, trust proxy fix

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const rateLimit  = require('express-rate-limit');
 const connectDB  = require('./config/db');
 
 const app = express();
+app.set('trust proxy', 1);
 
 app.use(helmet());
 app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
